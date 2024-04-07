04d07355bdb276fb55e40c5b36884a9bf4a97dbb
twebst <22550097+twebst@users.noreply.github.com>
2024-04-07 10:23:25 -0500

    Update configure.py
    add some comments

--- configure.py
+++ configure.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
+# adding comments
 """configure script to get build parameters from user."""
 
 import argparse
@@ -31,6 +33,8 @@ except ImportError:
   from distutils.spawn import find_executable as which
 # pylint: enable=g-import-not-at-top
 
+# to the config
+
 _DEFAULT_CUDA_VERSION = '11'
 _DEFAULT_CUDNN_VERSION = '2'
 _DEFAULT_TENSORRT_VERSION = '6'
@@ -44,6 +48,8 @@ _TF_BAZELRC_FILENAME = '.tf_configure.bazelrc'
 _TF_WORKSPACE_ROOT = ''
 _TF_BAZELRC = ''
 _TF_CURRENT_BAZEL_VERSION = None
+
+# asfalsyjdfvopasyidfgv9pasidf
 
 NCCL_LIB_PATHS = [
     'lib64/', 'lib/powerpc64le-linux-gnu/', 'lib/x86_64-linux-gnu/', ''
